dd5bcb28044f4b200c7c6602cbcac12eb99bc922
Gulshan Taghizada <103074001+taghizzada@users.noreply.github.com>
2025-11-01 20:54:49 +0100

    Add files via upload

---
+++ University Blog/scriptAB.js
@@ -0,0 +1,8 @@
+
+        const mobileMenuButton = document.getElementById('mobile-menu-button');
+        const mobileMenu = document.getElementById('mobile-menu');
+
+        mobileMenuButton.addEventListener('click', () => {
+            mobileMenu.classList.toggle('hidden');
+        });
+  
